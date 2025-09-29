e570286699060a23113eb24737d3c77045be2dcf
Lucas Martins <lucasmartinscaet@hotmail.com>
2025-09-29 16:40:38 -0300

    chore: removing unused imports from geist fonts from project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Lato, Poppins } from "next/font/google";
+import { Lato, Poppins } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 
