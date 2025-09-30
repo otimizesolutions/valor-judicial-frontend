97242753aaa0790b7184c1b8160ec7448bfa4d16
Lucas Martins <lucasmartinscaet@hotmail.com>
2025-09-30 18:38:42 -0300

    feat: adding hover pointer to button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,6 +1,6 @@
 export function Button({children, className}){
   return (
-    <button className={`px-4 py-3 rounded-lg ${className}`}>
+    <button className={`px-4 py-3 rounded-lg hover:cursor-pointer ${className}`}>
       {children}
     </button>
   )
