3fb4d249e5be5da8edd7f1136aaeb68b502b9ebf
Lucas Martins <lucasmartinscaet@hotmail.com>
2025-10-13 12:28:17 -0300

    feat: adding custom highlighted to dashboard

--- src/app/(private)/dashboard/components/highlighted-link.tsx
+++ src/app/(private)/dashboard/components/highlighted-link.tsx
@@ -1,16 +1,16 @@
-import { AlarmClockCheckIcon, ArrowRightIcon } from "lucide-react";
+import { ArrowRightIcon } from 'lucide-react'
 
-export function HighlightedLink(){
+export function HighlightedLink({ title, icon: Icon, quantity }) {
   return (
     <div className="px-6 py-4 border border-gray-100 rounded-lg">
       <div className="centered gap-2">
-        <AlarmClockCheckIcon />
-        <p className="text-gray-900 w-full">Processos em sua posse</p>
+        {Icon && (<Icon className="text-yellow-300" />)}
+        <p className="text-gray-900 w-full">{title}</p>
       </div>
-      
+
       <div className="mt-2 centered justify-between!">
-        <p className="font-semibold text-4xl text-gray-900">179</p>
-        <ArrowRightIcon className="size-10 p-2 rounded-full border border-blue-500 text-blue-500"/>
+        <p className="font-semibold text-4xl text-gray-900">{quantity}</p>
+        <ArrowRightIcon className="size-10 p-2 rounded-full border border-blue-500 text-blue-500" />
       </div>
 
     </div>
--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,17 +1,66 @@
-import { Header } from "./components/header";
-import { HighlightedLink } from "./components/highlighted-link";
+'use client'
 
+import { AlarmClock, ListFilterIcon, TriangleAlert } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { Header } from './components/header'
+import { HighlightedLink } from './components/highlighted-link'
+import { Table } from './components/table'
 
-export default function Dashboard(){
+export default function Dashboard() {
+  const links = [
+    {
+      title: 'Total de processos',
+      icon: null,
+      quantity: 179,
+      link: '#',
+    },
+    {
+      title: 'Processos em sua posse',
+      icon: null,
+      quantity: 40,
+      link: '#',
+    },
+    {
+      title: 'Prazos para hoje',
+      icon: TriangleAlert,
+      quantity: 7,
+      link: '#',
+    },
+    {
+      title: 'Pendências',
+      icon: AlarmClock,
+      quantity: 179,
+      link: '#',
+    },
+  ]
   return (
-    <div>
+    <div className="m-8">
       <Header />
 
       <section className="mt-6 grid lg:grid-cols-2 xl:grid-cols-4 gap-6">
-        <HighlightedLink />
-        <HighlightedLink />
-        <HighlightedLink />
-        <HighlightedLink />
+        {
+          links.map((link) => {
+            return (
+              <HighlightedLink
+                title={link.title}
+                icon={link.icon}
+                quantity={link.quantity}
+              />
+            )
+          })
+        }
+      </section>
+
+      <section className="mt-4">
+        <div className="mb-4">
+          <Button variant="secondary">
+            <ListFilterIcon />
+            Filtros
+          </Button>
+        </div>
+
+        <Table />
+
       </section>
 
     </div>
