c9939a61acc766d3bba5aae2519aec3b60cdc61a
Lucas Martins <lucasmartinscaet@hotmail.com>
2025-10-15 19:12:43 -0300

    feat: adding user info in dashboard and sidebar

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -1,25 +1,33 @@
-import { PlusIcon, ScrollTextIcon, UserRoundSearchIcon } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { PlusIcon, ScrollTextIcon, UserRoundSearchIcon } from 'lucide-react'
+import { useSession } from 'next-auth/react'
+import { Button } from '@/components/ui/button'
 
-export function Header(){
+export function Header() {
+  const { data } = useSession()
   return (
     <header className="flex items-center justify-between">
       <div>
-        <h1 className="title">Bem vindo, usuário !</h1>
+        <h1 className="title">
+          Bem vindo,
+          {' '}
+          {data?.user.name}
+          {' '}
+          !
+        </h1>
         <h2 className="subtitle">Acompanhe, gerencie seus clientes e processos.</h2>
       </div>
       <nav className=" centered gap-3">
         <Button>
           <ScrollTextIcon />
-          <p >Relatórios</p>
+          <p>Relatórios</p>
         </Button>
-        
+
         <Button>
           <UserRoundSearchIcon />
           <p>Consultar</p>
         </Button>
 
-        <Button variant={"secondary"}>
+        <Button variant="secondary">
           <PlusIcon />
           <p>Novo Processo</p>
         </Button>
